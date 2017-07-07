481dde0bc81afdb4af420282b1203b3bfc78179f
Chuck D'Antonio <c_dantonio@acm.org>
2017-07-07 13:52:01 -0400

    Changed return message

--- handler.js
+++ handler.js
@@ -20,7 +20,7 @@ module.exports.sendMessage = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Message sent',
       input: event,
     }),
   };
