a3890088b8f93d7cc160944c196f228e4476774f
Chuck D'Antonio <c_dantonio@acm.org>
2017-07-07 21:19:54 -0400

    Initial integration test deployment (doesn't test yet)

---
+++ ci/tasks/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+inputDir= 
+
+export AWS_ACCESS_KEY_ID=${aws_access_key_id}
+export AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -i | --input-dir )
+      inputDir=$2
+      shift
+      ;;
+    * )
+      echo "Unrecognized option: $1" 1>&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+error_and_exit() {
+  echo $1 >&2
+  exit 1
+}
+
+if [ ! -d "$inputDir" ]; then
+  error_and_exit "missing input directory: $inputDir"
+fi
+
+echo Deploying service...
+package=`find ${inputDir} -name '*.zip'`
+node ./node_modules/serverless/bin/serverless deploy --stage ${stage} --package ${package}
