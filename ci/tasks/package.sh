394eba3c13b3842333fee4b1f9327b0c51b7fb08
Chuck D'Antonio <c_dantonio@acm.org>
2017-07-07 20:56:45 -0400

    Removed env output

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -5,8 +5,6 @@ inputDir=  outputDir=  versionFile= service=
 export AWS_ACCESS_KEY_ID=${aws_access_key_id}
 export AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}
 
-env
-
 while [ $# -gt 0 ]; do
   case $1 in
     -i | --input-dir )
