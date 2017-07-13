40c93bde86a019e73b0233cdd84b501d1a95038e
Chuck D'Antonio <c_dantonio@acm.org>
2017-07-13 14:40:21 -0400

    Debugged integration testing script

--- test/integration.js
+++ test/integration.js
@@ -3,11 +3,11 @@ var AWS = require('aws-sdk');
 // you shouldn't hardcode your keys in production! See http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html
 AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, region: process.env.AWS_REGION});
 
-console.log("credentials: ${process.env.AWS_ACCESS_KEY_ID}:${process.env.AWS_SECRET_ACCESS_KEY}:${process.env.AWS_REGION}"); 
+console.log("credentials: " + process.env.AWS_ACCESS_KEY_ID + ":" + process.env.AWS_SECRET_ACCESS_KEY + ":" + process.env.AWS_REGION);
 
 var lambda = new AWS.Lambda();
 var params = {
-  FunctionName: "concourse-serverless-${process.env.stage}-sendMessage", /* required */
+  FunctionName: "concourse-serverless-" + process.env.stage + "-sendMessage" /* required */
 };
 lambda.invoke(params, function(err, data) {
   if (err) {
