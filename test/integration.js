06c20fee8190483255ed5f2ef3bc65db489af96f
Chuck D'Antonio <c_dantonio@acm.org>
2017-07-19 17:00:39 -0400

    Updated to use a proxy
    Updated to use a proxy for integration testing

--- test/integration.js
+++ test/integration.js
@@ -1,9 +1,16 @@
 var AWS = require('aws-sdk');
+var proxy = require('proxy-agent');
 
 // you shouldn't hardcode your keys in production! See http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html
 AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, region: process.env.AWS_REGION});
 
-console.log("credentials: " + process.env.AWS_ACCESS_KEY_ID + ":" + process.env.AWS_SECRET_ACCESS_KEY + ":" + process.env.AWS_REGION);
+if ( process.env.http_proxy ) {
+  AWS.config.update({
+    httpOptions: {
+      agent: proxy(process.env.http_proxy);
+    }
+  });
+}
 
 var lambda = new AWS.Lambda();
 var params = {
